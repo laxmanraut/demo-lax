aebf8eda83cfff8bcf3db5b670278c2917f7410a
laxmanraut <rautakshay136@gmail.com>
2017-11-10 00:00:47 +0530

    third commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Hapi = require('hapi');
 
-var server = new Hapi.Server(~~process.env.PORT || 3000, '0.0.0.0');
+var server = new Hapi.Server(process.env.PORT || 3000, '0.0.0.0');
 
 //server.connection({port: 5000});
 
